e17982b4f54e9fe1f83452094db53d8aad020294
bumis95 <bumis95@gmail.com>
2021-11-16 15:44:59 +0300

    Add firebase

--- app/src/main/java/com/example/mybooks/presentation/ui/OverviewFragment.kt
+++ app/src/main/java/com/example/mybooks/presentation/ui/OverviewFragment.kt
@@ -5,13 +5,12 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.example.mybooks.presentation.viewmodel.OverviewViewModel
 import com.example.mybooks.R
 import com.example.mybooks.data.books
 import com.example.mybooks.databinding.FragmentOverviewBinding
 import com.example.mybooks.presentation.adapter.BookAdapter
+import com.example.mybooks.presentation.viewmodel.OverviewViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class OverviewFragment : Fragment(R.layout.fragment_overview) {
@@ -23,15 +22,15 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fbAddBook.setOnClickListener {
+            viewModel.addToFirestore()
+        }
+
         binding.rvBooks.adapter = bookAdapter
         bookAdapter.submitList(books)
     }
 
     private fun openBookDetail(id: Long) {
 
-    }
-
-    companion object {
-        fun newInstance() = OverviewFragment()
     }
 }
--- app/src/main/java/com/example/mybooks/presentation/viewmodel/OverviewViewModel.kt
+++ app/src/main/java/com/example/mybooks/presentation/viewmodel/OverviewViewModel.kt
@@ -1,12 +1,31 @@
 package com.example.mybooks.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class OverviewViewModel @Inject constructor(
-
+    private val db: FirebaseFirestore
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun addToFirestore() {
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("books")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Timber.tag("FIRESTORE_TEST")
+                    .d("DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Timber.tag("FIRESTORE_TEST").d("Error adding document $e")
+            }
+    }
 }
