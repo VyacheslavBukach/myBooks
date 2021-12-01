fe3016a5a55bc401ff472338c2e5d3297044415c
bumis95 <bumis95@gmail.com>
2021-12-01 17:26:48 +0300

    Show whenlist is empty

--- app/src/main/java/com/example/mybooks/presentation/ui/books/OverviewFragment.kt
+++ app/src/main/java/com/example/mybooks/presentation/ui/books/OverviewFragment.kt
@@ -12,10 +12,12 @@ import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.mybooks.R
 import com.example.mybooks.databinding.FragmentOverviewBinding
+import com.example.mybooks.domain.entity.Book
 import com.example.mybooks.presentation.adapter.BookAdapter
 import com.example.mybooks.presentation.viewmodel.OverviewViewModel
 import com.example.mybooks.util.AuthState
 import com.example.mybooks.util.UiState
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -26,6 +28,7 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
     private val binding: FragmentOverviewBinding by viewBinding()
     private val viewModel by viewModels<OverviewViewModel>()
     private val bookAdapter = BookAdapter { openBookDetail(it.uuid, it.title, it.author) }
+    private var snackbar: Snackbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,14 +87,31 @@ class OverviewFragment : Fragment(R.layout.fragment_overview) {
                             when (uiState) {
                                 is UiState.Loading -> pbProgress.visibility = View.VISIBLE
                                 is UiState.Success -> {
-                                    pbProgress.visibility = View.INVISIBLE
+                                    pbProgress.visibility = View.GONE
+                                    checkEmptyList(uiState.data)
                                     bookAdapter.submitList(uiState.data)
                                 }
-                                is UiState.Failed -> pbProgress.visibility = View.INVISIBLE
+                                is UiState.Failed -> pbProgress.visibility = View.GONE
                             }
                         }
                     }
                 }
+            }
+        }
+    }
+
+    private fun checkEmptyList(data: List<Book>) {
+        with(binding) {
+            if (data == emptyList<Book>()) {
+                snackbar =
+                    Snackbar.make(requireView(), "Add some new books", Snackbar.LENGTH_INDEFINITE)
+                snackbar?.show()
+                ivError.visibility = View.VISIBLE
+            } else {
+                if (snackbar != null) {
+                    snackbar?.dismiss()
+                }
+                ivError.visibility = View.GONE
             }
         }
     }
